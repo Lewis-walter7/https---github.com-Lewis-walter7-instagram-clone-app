f24bd2d00642642c2c3902a230add14fa12eebc0
lewis <lewiswalter192@gmail.com>
2023-11-10 16:12:59 +0300

    sisth commit

--- app/hooks/usePost.ts
+++ app/hooks/usePost.ts
@@ -1,10 +1,11 @@
+import useSWR from "swr";
 import fetcher from "../lib/fetcher";
-import useSWR from 'swr'
 
-export default function usePost(postId: string){
+const usePost = (postId: string) =>{
     const { data, isLoading, error, mutate} = useSWR(`/api/post/${postId}`, fetcher)
-
     return {
-        data, isLoading, error,mutate
+        data, isLoading, mutate,error
     }
 }
+
+export default usePost;
