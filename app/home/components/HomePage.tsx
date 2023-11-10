bd163e3444b4cb2125a4050edad9d60a3134a564
lewis <lewiswalter192@gmail.com>
2023-11-10 15:46:54 +0300

    fifth commit

--- app/home/components/HomePage.tsx
+++ app/home/components/HomePage.tsx
@@ -1,9 +1,20 @@
+import PostCard from '@/app/components/PostCard'
+import usePosts from '@/app/hooks/usePosts'
+import { Post } from '@prisma/client'
 import React from 'react'
 
 const HomePage = () => {
-  return (
-    <div>HomePage</div>
-  )
+    const {data: homePosts, isLoading, error, mutate:mutateHomePosts} = usePosts()
+    return (
+      <div className='border-l border-gray-800 h-full overflow-y-scroll'>
+        {homePosts.map((item:Post) => (
+          <PostCard 
+              key={item.id}
+              item={item}
+          />
+        ))}
+      </div>
+    )
 }
 
 export default HomePage
