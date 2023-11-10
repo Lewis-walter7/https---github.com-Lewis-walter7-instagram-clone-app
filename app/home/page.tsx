5c0eb4e9c704ae7f9f2c47b466f61a826fbe54af
lewis <lewiswalter192@gmail.com>
2023-11-10 16:09:28 +0300

    sixth commit

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -1,10 +1,7 @@
 import React from 'react'
-import usePosts from '../hooks/usePosts'
 import HomePage from './components/HomePage'
 
 const page = () => {
-
-  const {data: homePosts, isLoading, error, mutate:mutateHomePosts} = usePosts()
   return (
     <div className='border-l border-gray-800 h-full overflow-y-scroll'>
       <HomePage />
