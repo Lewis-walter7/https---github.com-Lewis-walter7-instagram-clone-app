e5e0569a21bb6372ea71221372431370e94cb938
lewis <lewiswalter192@gmail.com>
2023-11-10 16:23:57 +0300

    sixth commit

--- app/hooks/usePosts.ts
+++ app/hooks/usePosts.ts
@@ -2,9 +2,9 @@ import fetcher from "../lib/fetcher";
 import useSWR from 'swr'
 
 const usePosts = () =>{
-    const { data, isLoading, error, mutate} = useSWR("/api/post", fetcher)
+    const { data, isLoading, error, mutate } = useSWR('/api/post', fetcher)
     return {
-        data, isLoading, mutate,error
+        data, isLoading, mutate, error
     }
 }
 
