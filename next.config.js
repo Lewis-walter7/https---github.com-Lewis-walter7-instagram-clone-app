ee4ed570bc617b2a961521fe0656c057e821a6d5
lewis <lewiswalter192@gmail.com>
2023-11-11 19:17:51 +0300

    sixth commit

--- next.config.js
+++ next.config.js
@@ -2,12 +2,19 @@
  // next.config.js
 
 module.exports = {
-    webpack: (config) => {
+    webpack: (config, { isServer }) => {
       // Add your webpack configurations here
       config.module.rules.push({
         test: /\.html$/,
         use: 'html-loader'
       });
+      if (!isServer) {
+        config.resolve.fallback = {
+            fs: false
+        }
+    }
+
+    return config;
   
       return config;
     },
