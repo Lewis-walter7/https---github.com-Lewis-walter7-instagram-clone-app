5b6a349a86d520ec856d438733936e33f564c381
lewis <lewiswalter192@gmail.com>
2023-11-12 08:50:56 +0300

    sixth commit

--- next.config.js
+++ next.config.js
@@ -1,6 +1,12 @@
  /** @type {import('next').NextConfig} */
  // next.config.jss
 
-const nextConfig = {}
+//const nextConfig = {}
 
-module.exports = nextConfig
+module.exports = {
+  module:{
+    loaders:[
+      { test: /\.html$/, loader: 'ignore-loader' }
+    ]
+  }
+}
